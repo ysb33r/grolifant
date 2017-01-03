e1e6d519fc21918c4e5bd2b29d23fbeb793a48ff
Schalk W. Cronjé <ysb33r@gmail.com>
2017-01-03 22:00:02 +0200

    Add OperatingSystem support (#5)
    - CHANGELOG updated
    - Removed dead comments
    - Updated class groovydoc

---
+++ src/main/groovy/org/ysb33r/gradle/olifant/internal/os/GenericBSD.groovy
@@ -0,0 +1,28 @@
+/*
+ * ============================================================================
+ * (C) Copyright Schalk W. Cronje 2016 - 2017
+ *
+ * This software is licensed under the Apache License 2.0
+ * See http://www.apache.org/licenses/LICENSE-2.0 for license details
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ * ============================================================================
+ */
+/*
+    This code is based upon code from the Gradle org.gradle.internal.os.OperatingSystem class
+    which is nder the Apache v2.0 license. Original copyright from 2010 remains. Modifications
+    from 2017+ are under the copyright and licensed mentioned above
+*/
+package org.ysb33r.gradle.olifant.internal.os
+
+/** Abstract base class for BSD-based operating systems.
+ *
+ */
+abstract class GenericBSD extends GenericUnix {
+    protected GenericBSD() {
+        super()
+    }
+}
---
+++ src/main/groovy/org/ysb33r/gradle/olifant/internal/os/GenericUnix.groovy
@@ -0,0 +1,122 @@
+/*
+ * ============================================================================
+ * (C) Copyright Schalk W. Cronje 2016 - 2017
+ *
+ * This software is licensed under the Apache License 2.0
+ * See http://www.apache.org/licenses/LICENSE-2.0 for license details
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ * ============================================================================
+ */
+/*
+    This code is based upon code from the Gradle org.gradle.internal.os.OperatingSystem class
+    which is nder the Apache v2.0 license. Original copyright from 2010 remains. Modifications
+    from 2017+ are under the copyright and licensed mentioned above
+*/
+package org.ysb33r.gradle.olifant.internal.os
+
+import groovy.transform.CompileStatic
+import org.ysb33r.gradle.olifant.OperatingSystem
+
+/** Generic Unix-like implementation of {@code OperatingSystem}.
+ *
+ * Also serves as a base class for specific Unix-like implementations.
+ */
+@CompileStatic
+class GenericUnix extends OperatingSystem {
+    static final OperatingSystem INSTANCE = new GenericUnix()
+
+    @Override
+    boolean isUnix() { true }
+
+    @Override
+    String getExecutableName(final String executablePath) {
+        executablePath
+    }
+
+    /** Returns OS-specific decorated script name.
+     *
+     * @param scriptPath Name of script
+     * @return Returns an appropriately decorated script name
+     */
+    @Override
+    String getScriptName(String scriptPath) {
+        scriptPath
+    }
+
+    /** Returns OS-specific shared library name
+     *
+     * @param libraryName This can be a base name or a full name.
+     * @return Shared library name.
+     */
+    @Override
+    String getSharedLibraryName(String libraryName) {
+        getLibraryName(libraryName, getSharedLibrarySuffix())
+    }
+
+    @Override
+    String getArch() {
+        if(OS_ARCH == 'x86_64') {
+            return 'amd64'
+        }
+
+        if (OS_ARCH == 'x86') {
+            return 'i386'
+        }
+
+        if (OS_ARCH == 'powerpc') {
+            return 'ppc'
+        }
+
+        'unknown'
+    }
+
+    /** Return Unix-like string that is used to suffix to shared libraries
+     *
+     * @return {@code .so}
+     */
+    @Override
+    String getSharedLibrarySuffix() {
+        '.so'
+    }
+
+    /** Default Unix-like string that is used to suffix to static libraries
+     *
+     * @return {@code .a}
+     */
+    @Override
+    String getStaticLibrarySuffix() {
+        '.a'
+    }
+
+    /** Returns OS-specific static library name
+     *
+     * @param libraryName This can be a base name or a full name.
+     * @return Static library name.
+     */
+    @Override
+    String getStaticLibraryName(String libraryName) {
+        getLibraryName(libraryName, staticLibrarySuffix)
+    }
+
+    protected GenericUnix() {
+        super()
+    }
+
+    private String getLibraryName(String libraryName, String suffix) {
+        if (libraryName.endsWith(suffix)) {
+            return libraryName
+        }
+
+        int pos = libraryName.lastIndexOf('/')
+        if (pos >= 0) {
+            return "${libraryName.substring(0, pos + 1)}lib${libraryName.substring(pos + 1)}${suffix}"
+        } else {
+            return "lib${libraryName}${suffix}"
+        }
+    }
+
+}

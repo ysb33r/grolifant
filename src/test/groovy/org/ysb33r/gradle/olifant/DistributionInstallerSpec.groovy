b715d16e4d0707913b3b85548facda91348fb949
Schalk W. Cronjé <ysb33r@gmail.com>
2017-07-28 13:48:42 +0200

    Added a test for MSI unpacking (#17)

--- src/test/groovy/org/ysb33r/gradle/olifant/DistributionInstallerSpec.groovy
+++ src/test/groovy/org/ysb33r/gradle/olifant/DistributionInstallerSpec.groovy
@@ -15,6 +15,7 @@ package org.ysb33r.gradle.olifant
 
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
+import org.ysb33r.gradle.olifant.errors.DistributionFailedException
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -116,6 +117,23 @@ class DistributionInstallerSpec extends Specification {
         thrown(IllegalArgumentException)
     }
 
+    void 'Unpacking an MSI'() {
+        given: 'A MSO distribution'
+        TestInstallerForMSI installer = new TestInstallerForMSI(project)
+
+        when: 'The MSI based distribution is downloaded'
+        File downloaded = installer.distributionRoot
+
+        then: 'The MSI distribution should be unpacked on Windows (or exception or other OS)'
+        if(installer.IS_WINDOWS) {
+            downloaded.exists()
+            downloaded.absolutePath.contains(distPathString(TestInstallerForVariousFormats.DISTPATH))
+            downloaded.absolutePath.endsWith("testdist-${TestInstallerForVariousFormats.DISTVER}")
+        } else {
+            thrown(DistributionFailedException)
+        }
+    }
+
     static class TestInstaller extends AbstractDistributionInstaller {
 
         // tag::test_installer[]
@@ -147,6 +165,17 @@ class DistributionInstallerSpec extends Specification {
         @Override
         URI uriFromVersion(String version) {
             TESTDIST_DIR.toURI().resolve("testdist-${DISTVER}.${ext}")
+        }
+    }
+
+    static class TestInstallerForMSI extends TestInstallerForVariousFormats {
+        TestInstallerForMSI(Project project) {
+            super ('msi',project )
+        }
+
+        @Override
+        protected void unpack(File srcArchive, File destDir) {
+            unpackMSI(srcArchive, destDir)
         }
     }
 
